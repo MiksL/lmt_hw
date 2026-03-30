0028c197d18dfc64e73ecfadcee5146dbf738e22
MiksL <50447221+MiksL@users.noreply.github.com>
2026-03-30 17:09:40 +0300

    Added more test scenarios

--- simulate.py
+++ simulate.py
@@ -1,20 +1,64 @@
 import requests
-import time
+import sys
 
 BASE_URL = "http://localhost:8000"
 
-threats = [ # Should be intercepted by a jet around 57.251040, 24.516280
+threats = [
+    # Heading towards Poligons
     {
-        "track_id": "TEST-THREAT",
+        "track_id": "Poligons-1",
+        "speed_ms": 900,
+        "altitude_m": 5000,
+        "heading_deg": 220,
+        "latitude": 57.4,
+        "longitude": 24.7,
+        "report_time": 1
+    },
+    # Heading towards RIX
+    {
+        "track_id": "RIX-1",
+        "speed_ms": 350,
+        "altitude_m": 3000,
+        "heading_deg": 245,
+        "latitude": 57.05,
+        "longitude": 24.6,
+        "report_time": 1
+    },
+    # Heading towards Riga HES
+    {
+        "track_id": "HES-1",
+        "speed_ms": 200,
+        "altitude_m": 800,
+        "heading_deg": 195,
+        "latitude": 57.05,
+        "longitude": 24.35,
+        "report_time": 1
+    },
+    # Heading towards Riga Base
+    {
+        "track_id": "BASE-1",
         "speed_ms": 600,
         "altitude_m": 1000,
         "heading_deg": 212,
         "latitude": 57.5,
         "longitude": 24.8,
         "report_time": 1
-    }
+    },
+    # Heading towards poligons
+    {
+        "track_id": "Poligons-2",
+        "speed_ms": 1500,
+        "altitude_m": 15000,
+        "heading_deg": 210,
+        "latitude": 57.9,
+        "longitude": 25.2,
+        "report_time": 1
+    },
 ]
 
+selected = sys.argv[1:] if len(sys.argv) > 1 else [t["track_id"] for t in threats]
+
 for threat in threats:
-    r = requests.post(f"{BASE_URL}/api/radar", json=threat)
-    print(f"[SIM] {threat['track_id']} — {r.status_code}")
+    if threat["track_id"] in selected:
+        r = requests.post(f"{BASE_URL}/api/radar", json=threat)
+        print(f"[SIM] {threat['track_id']} — {r.status_code}")
